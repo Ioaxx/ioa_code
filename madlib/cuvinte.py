05e2daa3cffa7803a4c5cffd66702d91bae57e8f
Ioana Nistor <ioananistor36@gmail.com>
2022-07-27 09:05:06 +0300

    stat_unfinish

--- madlib/cuvinte.py
+++ madlib/cuvinte.py
@@ -7,30 +7,24 @@ with open("reduceri.txt") as data_file:
 '''
 
         
-from matplotlib.cbook import index_of
+#from matplotlib.cbook import index_of
 
 
-with open("madlib\\reduceri.txt") as f:
-    lines = f.readlines()
-    #print (lines)
-    for line in lines:
-        cuv = line.split()
-        print (cuv)
-        contor=0
-        for litera in cuv:
-            first_char = ""
-            for i in range(0, 1):
-                first_char = first_char + litera[i]
-            #print("First character:", first_char)
-            '''
+f= open("madlib\\reduceri.txt") 
+lines = f.readlines()
+#print (lines)
+for line in lines:
+    cuv = line.split()
+    print (cuv)
+    contor=0
+    for litera in cuv:
+        #print("First character:", first_char)
+        #if first_char is "<":
+        if litera.startswith("<"):
+            #print("write a:", lines[first_ct:last_ct])
+            print("write a", litera[0:litera.find(">")+1])
+            input()
             
-            '''
-            #if first_char is "<":
-            if litera.startswith("<"):
-                #print("write a:", lines[first_ct:last_ct])
-                print("write a", litera[0:litera.find(">")+1])
-                input()
-        
         
         #............
         
---
+++ stats/dic_stat.py
@@ -0,0 +1,25 @@
+f= open("stats\\words.txt")
+dictionar = {}
+lines = f.readlines()
+for line in lines:
+    cuv = line.split()
+    for i in range(1) :
+        dictionar[cuv[0]]=cuv[1]
+#print(dictionar)
+
+litere=["abcdefghijklmnopqrstuvwxyz"]
+sorted(dictionar.values())
+print(dictionar)
+def starts_in ():
+    #for i in range(len(litere))
+    #for cuv in dictionar
+     contor=0
+     for cuv in dictionar.keys:
+        contor_litere=0
+        if cuv[0] == litere[contor]:
+            contor_litere+=1
+        else :
+            print(contor_litere, "incep cu ", litere[contor] )
+            contor+=1
+            contor_litere=0   
+starts_in()
